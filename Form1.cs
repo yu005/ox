0ba897c85bc90b4f79a0325ee01166fdc71c79cf
s1122100@o365st.pu.edu.tw <s1122100@o365st.pu.edu.tw>
2024-03-26 17:10:47 +0800

    4

--- Form1.cs
+++ Form1.cs
@@ -29,36 +29,6 @@ namespace ox
         {
 
         }
-
-
-        private void checkWinner()
-        {
-            if (button1.Text == button2.Text && button2.Text == button3.Text)
-            {
-                MessageBox.Show(button1.Text, "Winner is...");
-            }
-            if (button4.Text == button5.Text && button5.Text == button6.Text)
-            {
-                MessageBox.Show(button1.Text, "Winner is...");
-                return;
-            }
-            if (button7.Text == button8.Text && button8.Text == button9.Text)
-            {
-                MessageBox.Show(button1.Text, "Winner is...");
-                return;
-            }
-            if (button1.Text == button5.Text && button5.Text == button9.Text)
-            {
-                MessageBox.Show(button1.Text, "Winner is...");
-                return;
-            }
-            if (button3.Text == button5.Text && button5.Text == button7.Text)
-            {
-                MessageBox.Show(button1.Text, "Winner is...");
-                return;
-            }
-        }
-
         private void button1_Click_1(object sender, EventArgs e)
         {
             //避免下第二次
@@ -69,6 +39,58 @@ namespace ox
             //
             checkWinner();
         }
+
+        private void checkWinner()
+        {
+            if (button1.Text == button2.Text && button2.Text == button3.Text && button1.Text == playermMarker)
+            {
+                MessageBox.Show(button1.Text, "Winner is...");
+                return;
+            }
+            else if (button4.Text == button5.Text && button5.Text == button6.Text && button4.Text == playermMarker)
+            {
+                MessageBox.Show(button4.Text, "Winner is...");
+                return;
+            }
+            else if (button7.Text == button8.Text && button8.Text == button9.Text && button7.Text == playermMarker)
+            {
+                MessageBox.Show(button7.Text, "Winner is...");
+                return;
+            }
+            else if (button1.Text == button5.Text && button5.Text == button9.Text && button1.Text == playermMarker)
+            {
+                MessageBox.Show(button1.Text, "Winner is...");
+                return;
+            }
+            else if (button3.Text == button5.Text && button5.Text == button7.Text && button3.Text == playermMarker)
+            {
+                MessageBox.Show(button3.Text, "Winner is...");
+                return;
+            }
+            else if (button2.Text == button5.Text && button5.Text == button8.Text && button2.Text == playermMarker)
+            {
+                MessageBox.Show(button2.Text, "Winner is...");
+                return;
+            }
+            else if (button1.Text == button4.Text && button4.Text == button7.Text && button1.Text == playermMarker)
+            {
+                MessageBox.Show(button1.Text, "Winner is...");
+                return;
+            }
+            else if (button3.Text == button6.Text && button6.Text == button9.Text && button3.Text == playermMarker)
+            {
+                MessageBox.Show(button3.Text, "Winner is...");
+                return;
+
+            }
+            
+        }
+        //private void SomeEventTrigger_Click(object sender, EventArgs e)
+        //{
+        //    checkWinner();
+        //}
+
+
 
 
 
